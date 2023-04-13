9afee8b8f9d9b45809fa4f2e50c77d22bb049c97
David Morroni <morronini@gmail.com>
2023-04-13 15:11:13 +0200

    init

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request
+import argparse
+
+app = Flask(__name__)
+
+
+@app.route('/userinput', methods=['GET', 'POST'])
+def userinput():
+    if request.method == 'POST':
+        form_data = request.form.to_dict()
+        reusable_or_non_reusable = perform_calcs(form_data)
+        replacements = {"reusable_or_non_reusable": reusable_or_non_reusable}
+        return render_template('result.html', **replacements)
+    else:
+        return render_template('userinput_custom.html')
+
+def perform_calcs(form_data):
+    print(form_data)
+    if form_data["is_reusable"] == "Yes":
+        return "REUSABLE"
+    elif form_data["is_reusable"] == "No":
+        return "NOT REUSABLE"
+    else:
+        return "WE DON'T KNOW"
+
+def parseArguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--deploy", help="In testing just input No", type=str, default="No")
+    arguments = parser.parse_args()
+    return arguments
+
+if __name__ == "__main__":
+  args = parseArguments()
+  if args.deploy == "No":
+    app.run(debug=True)
+  else:
+    from waitress import serve
+    serve(app, host="0.0.0.1", port=8081)
+
